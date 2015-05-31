c6b214398a246214c40ddf343b61d6171fd0a14d
pcmagas <pc_magas@yahoo.gr>
2015-05-31 08:55:48 +0300

    More improvements

--- application/controllers/Donation.php
+++ application/controllers/Donation.php
@@ -15,7 +15,7 @@ class Donation extends CI_Controller
 		$nonce=$this->input->post('payment_method_nonce');
 		$oid=$this->input->post('oid');
 		$mid=$this->input->post('mid');
-		if(!empty($nonce))
+		if(!empty($nonce)&&!empty($oid)&&!empty($mid))
 		{
 			//Here we get somehow the id
 			$this->donation_model->donate_to($oid,$mid,$nonce);
